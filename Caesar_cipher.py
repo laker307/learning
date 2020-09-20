1d70d2df271d4c35b4e5b8ed5654a7a836adf3aa
laker307 <laker307@yandex.ru>
2020-09-20 17:03:48 +0500

    Add the main code

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -1,1 +1,65 @@
 import sys
+
+class process():
+
+    alphabetBIG = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЪЭЮЯ'
+    alphabetLITTLE = 'абвгдеёжзийклмнопрстуфхцчшщьъэюя'
+    new = str()
+
+    def __init__(self, name, count):
+        self.name = name
+        self.shift = count
+
+    def encode(self):
+        for i in self.name:
+            if i in process.alphabetBIG:
+                index = process.alphabetBIG.index(i)
+                process.new += process.alphabetBIG[index + self.shift]
+            elif i in process.alphabetLITTLE:
+                index = process.alphabetLITTLE.index(i)
+                process.new +=  process.alphabetLITTLE[index + self.shift]
+            else:
+                process.new = process.new + i
+        print(process.new)
+        process.new = ''
+
+    def decode(self):
+        for i in self.name:
+            if i in process.alphabetBIG:
+                index = process.alphabetBIG.index(i)
+                process.new += process.alphabetBIG[index - self.shift]
+            elif i in process.alphabetLITTLE:
+                index = process.alphabetLITTLE.index(i)
+                process.new += process.alphabetLITTLE[index - self.shift]
+            else:
+                process.new = process.new + i
+        print(process.new)
+        process.new = ''
+
+
+
+if __name__ == '__main__':
+    while True:
+
+        print('\n1 - encode\n'
+              '2 - decode\n')
+
+        command = input()
+
+        if command == '1':
+            print('Enter text')
+            text = input()
+            print('Enter count of shift')
+            sh = int(input())
+            proc = process(text,sh)
+            proc.encode()
+
+
+        if command == '2':
+            print('Enter text')
+            text = input()
+            print('Enter count of shift')
+            sh = int(input())
+            proc = process(text,sh)
+            proc.decode()
+
